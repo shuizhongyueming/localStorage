7275a2646536e94ad64d276023e4170b4b3fa4db
WangXing <shuizhongyueming@gmail.com>
2013-10-10 11:59:08 +0800

    构建目录结构，完成不跨域和支持localStorage情况下，getItem功能

---
+++ js/localStorage.js
@@ -0,0 +1,21 @@
+ndow.qike = window.qike || {};
+window.qike.localStorage = {};
+window.qike.localStorage.isSupportLocalStorage=(function(){
+    var m = this;
+    try{
+        return 'localStorage' in window && window['localStorage'] !== null;
+    }catch(e){
+        return false;
+    }
+})();
+qike.localStorage.currDomain=location.protocol+'//'+location.hostname+(location.port&&':'+location.port);
+qike.localStorage.getItem = function(key,domain,callback){
+    var m = this,res;     
+    if(m.isSupportLocalStorage){
+        // √¥µ”√ª’”√”µ«“÷µ ∫£÷Ωª»
+        if(!domain || domain===m.currDomain){
+            res = localStorage.getItem(key);
+            typeof callback === 'function' && callback();
+            return res;
+	}
+    }												    };
