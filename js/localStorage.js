0d5f4c9512fcd19c12316591cc7944c97c5a3054
WangXing <shuizhongyueming@gmail.com>
2013-10-15 11:41:37 +0800

    对于原生的localStorage，实现了跨域获取本地数据的功能

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,5 +1,6 @@
 window.qike = window.qike || {};
 window.qike.localStorage = {};
+window.qike.isIE = /*@cc_on !@*/false;
 window.qike.localStorage.isSupportLocalStorage=(function(){
     var m = this;
     try{
@@ -8,11 +9,19 @@ window.qike.localStorage.isSupportLocalStorage=(function(){
         return false;
     }
 })();
-qike.localStorage.loadIframe=function(domain,callback){
+qike.localStorage.loadIframe=function(domain,obj){
     var m = this,d=document,iframe;
     m[domain]={};
     // 存储在加载过程中，收集到的操作请求，加载完成之后，遍历运行
+    // {
+    //  action: getItem,
+    //  key: 'test',
+    //  value: null,
+    //  index: null,
+    //  callback: callback
+    // }
     m[domain].actionArr=[];
+    m[domain].actionArr.push(obj);
     // isLoading  1 ==> 正在加载  2 ==> 加载完毕 undefined ==> 未加载
     m[domain].isLoading=1;
     iframe=d.createElement('iframe');
@@ -22,11 +31,45 @@ qike.localStorage.loadIframe=function(domain,callback){
     iframe.src=domain+'GitHub/localStorage/localStorage.html';
     m[domain].dom = iframe;
     d.getElementsByTagName('body')[0].appendChild(iframe);
-    m[domain].dom.onload=function(e){
+    
+    if(window.addEventListener){
+        iframe.addEventListener('load',function(e){
+            callback();
+        });
+    }else{
+        iframe.attachEvent('onload',function(){
+            callback();
+        });
+    }
 
-    };
+    // 在IFrame加载完成之后的遍历执行
+    function callback(){
+        var arr=m[domain].actionArr,
+            len=arr.length,
+            i=0,
+            win,
+            curr,res;
+        // 标识IFrame加载完成
+        m[domain].isLoading=2;
+        // 存储在IFrame里面的localStorage 但凡使用IFrame的，必然是支持localStorage的场景
+        iframeLocalStorage=m[domain].iframeLocalStorage=iframe.contentWindow.localStorage;
+
+        for(i;i<len;i++){
+            curr = arr[i];
+            if(curr.action === 'getItem' || curr.action === 'removeItem'){
+                res=iframeLocalStorage[curr.action](curr.key);
+            }else if(curr.action === 'key'){
+                res=iframeLocalStorage.key(curr.key);
+            }else if(curr.action === 'clear'){
+                res=iframeLocalStorage.clear();
+            }
+            typeof curr.callback === 'function' && curr.callback(res);
+        }
+    }
 };
+
 qike.localStorage.currDomain = location.protocol+'//'+location.hostname+(location.port&&':'+location.port)+'/';
+
 qike.localStorage.getItem = function(key,domain,callback){
     var m = this,res;
     
@@ -51,11 +94,25 @@ qike.localStorage.getItem = function(key,domain,callback){
             
             // 域名与当前的相同
             res = localStorage.getItem(key);
-            typeof callback === 'function' && callback();
+            typeof callback === 'function' && callback(res);
             return res;
         }else{
 
             // 域名与当前的不相同
+            if(m[domain] === undefined) m[domain]={};
+            switch(m[domain].isLoading){
+                case 1:
+                    // 正在加载，则把操作放到队列中
+                    m[domain].actionArr.push({action:'getItem',key:key,callback:callback});
+                    break;
+                case 2:
+                    // 加载完毕，则直接操作
+                    res=m[domain].iframeLocalStorage.getItem(key);
+                    typeof callback === 'function' && callback(res);
+                    return res;
+                default:
+                    m.loadIframe(domain,{action:'getItem',key:key,callback:callback});
+            }
         }
     }
 };
