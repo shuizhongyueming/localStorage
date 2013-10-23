0ef1e74bd657d393e2c2f65ca862fabd9a3fcf42
WangXing <shuizhongyueming@gmail.com>
2013-10-23 22:13:41 +0800

    优化代码;去掉console.lo

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,19 +1,24 @@
 window.qike = window.qike || {};
 window.qike.localStorage = {};
 window.qike.localStorage.isIE = /*@cc_on !@*/false;
-window.qike.localStorage.isSupportLocalStorage=false;
-// (function(){
-//     var m = this;
-//     try{
-//         return 'localStorage' in window && window['localStorage'] !== null;
-//     }catch(e){
-//         return false;
-//     }
-// })();
+
+// domain+baseHtmlUrl ==> 跨域文件的地址
+window.qike.localStorage.baseHtmlUrl='localStorage.html';
+
+// domain+baseFlashUrl ==> Flash的地址
+window.qike.localStorage.baseFlashUrl='GitHub/localStorage/swf/saveLocalData20131015.swf';
+
+window.qike.localStorage.isSupportLocalStorage=(function(){
+    var m = this;
+    try{
+        return 'localStorage' in window && window['localStorage'] !== null;
+    }catch(e){
+        return false;
+    }
+})();
 
 // 跨域获取LocalStorage数据时，加载对应域名下的文件
 qike.localStorage.loadIframe=function(domain,obj){
-    console.log('function loadIframe')
     var m = this,d=document,iframe;
     m[domain]={};
     // 存储在加载过程中，收集到的操作请求，加载完成之后，遍历运行
@@ -28,30 +33,23 @@ qike.localStorage.loadIframe=function(domain,obj){
     m[domain].actionArr.push(obj);
     // isLoading  1 ==> 正在加载  2 ==> 加载完毕 undefined ==> 未加载
     m[domain].isLoading=1;
-    console.log('createElement iframe')
     iframe=d.createElement('iframe');
     iframe.width='0';
     iframe.height='0';
-    iframe.frameborder='0';
-    iframe.src=domain+'localStorage.html';
+    iframe.frameBorder='0';
+    iframe.src=domain+m.baseHtmlUrl;
     m[domain].dom = iframe;
     d.getElementsByTagName('body')[0].appendChild(iframe);
-    console.log('append iframe')
     
     // 支持localStorage的时候，是由iframe的onload事件来知晓是否加载完毕。
-    // 不支持localStorage的时候，iframe内部会加载flash，
-    // 在flash加载完毕的时候，会根据flashvars里面的参数回调父级的flashCallback
-    if(m.isSupportLocalStorage){
-        console.log('isSupportLocalStorage')
-        if(window.addEventListener){
-            iframe.addEventListener('load',function(e){
-                callback();
-            });
-        }else{
-            iframe.attachEvent('onload',function(){
-                callback();
-            });
-        }
+    if(window.addEventListener){
+        iframe.addEventListener('load',function(e){
+            callback();
+        });
+    }else{
+        iframe.attachEvent('onload',function(){
+            callback();
+        });
     }
 
     // 在IFrame加载完成之后的遍历执行
@@ -100,7 +98,6 @@ qike.localStorage.loadFlash=function(domain,obj){
         swfName,
         flashvars;
 
-    console.log('function loadFlash');
 
     m[domain]={};
 
@@ -110,13 +107,11 @@ qike.localStorage.loadFlash=function(domain,obj){
     // isLoading  1 ==> 正在加载  2 ==> 加载完毕 undefined ==> 未加载
     m[domain].isLoading=1;
 
-    console.log('createElement div')
     
     div=d.createElement('div');
     d.body.appendChild(div);
     
 
-    console.log('hide div')
 
     div.style.position = 'absolute';
     div.style.top = '-2000px';
@@ -124,13 +119,11 @@ qike.localStorage.loadFlash=function(domain,obj){
 
 
 
-    console.log('createElement div and hide it finish')
 
     swfName = 'SwfStore_'+(new Date().valueOf());
-    flashvars = 'domain='+domain+'&amp;'+'callback=window.parent.qike.localStorage.flashCallback';
-    swfUrl = domain+'GitHub/localStorage/swf/saveLocalData20131015.swf';
+    flashvars = 'domain='+domain+'&amp;'+'callback=qike.localStorage.flashCallback';
+    swfUrl = domain+m.baseFlashUrl;
 
-    console.log('put flash string into div')
 
     div.innerHTML = '<object height="100" width="500" codebase="https://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab" id="' +
                         swfName + '" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">' +
@@ -142,7 +135,6 @@ qike.localStorage.loadFlash=function(domain,obj){
                         'name="' + swfName + '" bgcolor="#ffffff" src="' + swfUrl + '">' +
                         '</object>';
 
-    console.log('get the flash dom')
 
     m[domain].swf = document[swfName] || window[swfName];
 
@@ -150,7 +142,6 @@ qike.localStorage.loadFlash=function(domain,obj){
 
 qike.localStorage.flashCallback=function(domain){
     // alert('aa')
-    console.log('flash is ready')
     var m = this,
         arr=m[domain].actionArr,
         len=arr.length,
@@ -181,22 +172,16 @@ qike.localStorage.flashCallback=function(domain){
         for(i;i<len;i++){
             curr = arr[i];
             if(curr.action === 'getItem'){
-                console.log('getItem:'+curr.key);
                 res=flashLocalStorage.getItem(curr.key);
             }else if(curr.action === 'setItem'){
-                console.log('setItem:'+curr.key+'='+curr.value);
                 res=flashLocalStorage.setItem(curr.key,curr.value);
             }else if(curr.action === 'removeItem'){
-                console.log('removeItem:'+curr.key);
                 res=flashLocalStorage.removeItem(curr.key);
             }else if(curr.action === 'key'){
-                console.log('key:'+curr.index);
                 res=flashLocalStorage.key(curr.index);
             }else if(curr.action === 'clear'){
-                console.log('clear');
                 res=flashLocalStorage.clear();
             }
-            console.log('res='+res)
             typeof curr.callback === 'function' && curr.callback(res);
         }
     },1);
@@ -204,25 +189,33 @@ qike.localStorage.flashCallback=function(domain){
 
 qike.localStorage.currDomain = location.protocol+'//'+location.hostname+(location.port&&':'+location.port)+'/';
 
-qike.localStorage.commonAction = function(){
-    
-};
+/**
+ * [commonAction 实现级接口]
+ * @param  {[object]} obj {action: 'getItem',key:'key',value:'value',index:'index',callback:'cbk'}
+ */
+qike.localStorage.commonAction = function(obj){
+    var m = this,res,args,domain;
 
-qike.localStorage.getItem = function(key,domain,callback){
-    var m = this,res;
-    
+    switch(obj.action){
+        case 'getItem': args=[obj.key];break;
+        case 'setItem': args=[obj.key,obj.value];break;
+        case 'removeItem': args=[obj.key];break;
+        case 'key': args=[obj.index];break;
+        case 'clear': args=[];break;
+    }
+
+
     // 格式化domain
-    domain = m.formatDomain(domain);
-    console.log('finish format domain')
-    
+    domain = m.formatDomain(obj.domain);
 
     // 支持localStorage
     if(m.isSupportLocalStorage){
+
         if(domain===m.currDomain){
             
             // 域名与当前的相同
-            res = localStorage.getItem(key);
-            typeof callback === 'function' && callback(res);
+            res = localStorage[obj.action].apply(localStorage,args);
+            typeof obj.callback === 'function' && obj.callback.call(window,res);
             return res;
         }else{
 
@@ -231,210 +224,61 @@ qike.localStorage.getItem = function(key,domain,callback){
             switch(m[domain].isLoading){
                 case 1:
                     // 正在加载，则把操作放到队列中
-                    m[domain].actionArr.push({action:'getItem',key:key,callback:callback});
+                    m[domain].actionArr.push(obj);
                     return;
                 case 2:
                     // 加载完毕，则直接操作
-                    res=m[domain].iframeLocalStorage.getItem(key);
-                    typeof callback === 'function' && callback(res);
+                    res=m[domain].iframeLocalStorage[obj.action].apply(m[domain].iframeLocalStorage,args);
+                    typeof obj.callback === 'function' && obj.callback.call(window,res);
                     return res;
                 default:
-                    m.loadIframe(domain,{action:'getItem',key:key,callback:callback});
+                    m.loadIframe(domain,obj);
                     return;
             }
         }
     }
 
+
     // 不支持localStorage，使用FlashCookie
     // 针对FlashCookie，不管任何域名都是需要加载对应域名下的swf文件
     if(!m.isSupportLocalStorage){
-        console.log('unsupport localStorage')
         if(m[domain] === undefined) m[domain]={};
 
         switch(m[domain].isLoading){
             case 1:
                 // 正在加载，则把操作放到队列中
-                console.log('flash is loading');
-                m[domain].actionArr.push({action:'getItem',key:key,callback:callback});
+                m[domain].actionArr.push(obj);
                 return;
             case 2:
                 // 加载完毕，则直接操作
-                console.log('flash is loaded');
-                res=m[domain].swf.getItem(key);
-                typeof callback === 'function' && callback(res);
+                res=m[domain].swf[obj.action].apply(m[domain].swf,args);;
+                typeof obj.callback === 'function' && obj.callback.call(window,res);
                 return res;
             default:
-                console.log('flash is unload');
-                m.loadFlash(domain,{action:'getItem',key:key,callback:callback});
+                m.loadFlash(domain,obj);
                 return;
         }
     }
+};
+qike.localStorage.getItem = function(key,domain,callback){
+    var m = this;
+    m.commonAction({action:'getItem',key:key,domain:domain,callback:callback});
 };
 qike.localStorage.setItem = function(key,value,domain,callback){
-    var m = this,res;
-    
-    // 格式化domain
-    domain = m.formatDomain(domain);
-
-    // 支持localStorage
-    if(m.isSupportLocalStorage){
-        
-        // 域名与当前的相同
-        if(domain===m.currDomain){
-            res  = localStorage.setItem(key,value);
-            typeof callback === 'function' && callback();
-            return res;
-        }else{
-
-            // 域名与当前的不相同
-            if(m[domain] === undefined) m[domain]={};
-            switch(m[domain].isLoading){
-                case 1:
-                    // 正在加载，则把操作放到队列中
-                    m[domain].actionArr.push({action:'setItem',key:key,value:value,callback:callback});
-                    return;
-                case 2:
-                    // 加载完毕，则直接操作
-                    res=m[domain].iframeLocalStorage.setItem(key,value);
-                    typeof callback === 'function' && callback(res);
-                    return res;
-                default:
-                    m.loadIframe(domain,{action:'setItem',key:key,value:value,callback:callback});
-                    return;
-            }
-        }
-    }
-
-    // 不支持localStorage，使用FlashCookie
-    // 针对FlashCookie，不管任何域名都是需要加载对应域名下的swf文件
-    if(!m.isSupportLocalStorage){
-        console.log('unsupport localStorage')
-        if(m[domain] === undefined) m[domain]={};
-
-        switch(m[domain].isLoading){
-            case 1:
-                // 正在加载，则把操作放到队列中
-                console.log('flash is loading');
-                m[domain].actionArr.push({action:'setItem',key:key,value:value,callback:callback});
-                return;
-            case 2:
-                // 加载完毕，则直接操作
-                console.log('flash is loaded');
-                res=m[domain].swf.setItem(key,value);
-                typeof callback === 'function' && callback(res);
-                return res;
-            default:
-                console.log('flash is unload');
-                m.loadFlash(domain,{action:'setItem',key:key,value:value,callback:callback});
-                return;
-        }
-    }
-
-
+    var m = this;
+    m.commonAction({action:'setItem',key:key,value:value,domain:domain,callback:callback});
 };
 qike.localStorage.removeItem = function(key,domain,callback){
-    var m = this,res;
-
-    // 格式化domain
-    domain = m.formatDomain(domain);
-
-    // 支持localStorage
-    if(m.isSupportLocalStorage){
-        // 域名没设定或者与当前的相同
-        if(!domain || domain===m.currDomain){
-            res = localStorage.removeItem(key);
-            typeof callback === 'function' && callback();
-            return res;
-        }else{
-
-            // 域名与当前的不相同
-            if(m[domain] === undefined) m[domain]={};
-            switch(m[domain].isLoading){
-                case 1:
-                    // 正在加载，则把操作放到队列中
-                    m[domain].actionArr.push({action:'removeItem',key:key,callback:callback});
-                    return;
-                case 2:
-                    // 加载完毕，则直接操作
-                    res=m[domain].iframeLocalStorage.removeItem(key);
-                    typeof callback === 'function' && callback(res);
-                    return res;
-                default:
-                    m.loadIframe(domain,{action:'removeItem',key:key,callback:callback});
-                    return;
-            }
-        }
-    }
+    var m = this;
+    m.commonAction({action:'removeItem',key:key,domain:domain,callback:callback});
 };
 qike.localStorage.key=function(index,domain,callback){
-    var m = this,res;
-
-    // 格式化domain
-    domain = m.formatDomain(domain);
-
-    // 支持localStorage
-    if(m.isSupportLocalStorage){
-        // 域名没设定或者与当前的相同
-        if(!domain || domain===m.currDomain){
-            res = localStorage.key(index);
-            typeof callback === 'function' && callback();
-            return res;
-        }else{
-
-            // 域名与当前的不相同
-            if(m[domain] === undefined) m[domain]={};
-            switch(m[domain].isLoading){
-                case 1:
-                    // 正在加载，则把操作放到队列中
-                    m[domain].actionArr.push({action:'key',index:index,callback:callback});
-                    return;
-                case 2:
-                    // 加载完毕，则直接操作
-                    res=m[domain].iframeLocalStorage.key(index);
-                    typeof callback === 'function' && callback(res);
-                    return res;
-                default:
-                    m.loadIframe(domain,{action:'key',index:index,callback:callback});
-                    return;
-            }
-        }
-    }
+    var m = this;
+    m.commonAction({action:'key',index:index,domain:domain,callback:callback});
 };
 qike.localStorage.clear=function(domain,callback){
-    var m = this,res;
-
-    // 格式化domain
-    domain = m.formatDomain(domain);
-
-    // 支持localStorage
-    if(m.isSupportLocalStorage){
-        // 域名没设定或者与当前的相同
-        if(!domain || domain===m.currDomain){
-            res = localStorage.clear();
-            typeof callback === 'function' && callback();
-            return res;
-        }else{
-
-            // 域名与当前的不相同
-            if(m[domain] === undefined) m[domain]={};
-
-            switch(m[domain].isLoading){
-                case 1:
-                    // 正在加载，则把操作放到队列中
-                    m[domain].actionArr.push({action:'clear',callback:callback});
-                    return;
-                case 2:
-                    // 加载完毕，则直接操作
-                    res=m[domain].iframeLocalStorage.clear();
-                    typeof callback === 'function' && callback(res);
-                    return res;
-                default:
-                    m.loadIframe(domain,{action:'clear',callback:callback});
-                    return;
-            }
-
-        }
-    }
+    var m = this;
+    m.commonAction({action:'clear',domain:domain,callback:callback});
 };
 
 // 按照规则格式化传进来的域名
@@ -456,5 +300,5 @@ qike.localStorage.formatDomain=function(domain){
         domain = domain+'/';
     }
 
-    return domain;
+    return domain.toLowerCase();
 }
